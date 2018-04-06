9a7f29510a4e2796d2f77f54a4f49cab24c63000
margogrand <margogrand@dhcp-18-179.invite.emn.fr>
2018-04-06 18:10:24 +0200

    margaux

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/groupe7.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/groupe7.java
@@ -4,5 +4,6 @@ public class groupe7 {
 	public static void main(String[] args) {
 		System.out.println("<leo80250> L�o FARGEAS");
 		System.out.println("<MaelleBoulard> Maelle Boulard");
+		System.out.println("<margauxgrand> Margaux Grand");
 	}
 }
